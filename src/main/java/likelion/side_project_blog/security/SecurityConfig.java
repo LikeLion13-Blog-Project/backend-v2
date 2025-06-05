3639728d152cb9e25859316c7107e6b660479311
naaadang <naaadang@naver.com>
2025-06-05 09:58:36 +0900

    remove filter test

--- src/main/java/likelion/side_project_blog/security/SecurityConfig.java
+++ src/main/java/likelion/side_project_blog/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
